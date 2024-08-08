85b8432f28cff2c795fcf43ca9b0fdbf66d1df9f
Neel <neelp0980@gmail.com>
2024-08-08 16:53:39 +0530

    style: add navbar

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -6,6 +6,7 @@ import { Input } from "./ui/input";
 import { UnifiedWalletButton } from "@jup-ag/wallet-adapter";
 import Blinks from "./Blinks";
 import { validateURL } from "@/lib/helpers";
+import Image from "next/image";
 
 const Home = () => {
   const [url, setUrl] = useState<string>("");
@@ -28,7 +29,17 @@ const Home = () => {
 
   return (
     <div className='flex flex-col w-full'>
-      <div className='flex justify-end px-4 py-2 w-full h-fit'>
+      <div className='flex justify-between px-5 md:px-10 py-4 w-full h-fit'>
+        <span className="flex gap-2 items-center text-xl tracking-wide">
+        <Image
+          src='/debugger.png'
+          alt='Debugger'
+          width={35}
+          height={35}
+          className="w-9 h-7"
+        />
+        Blinks Debugger
+        </span>
         <UnifiedWalletButton />
       </div>
       <div className='flex items-center justify-center gap-2 mt-8 w-full'>
@@ -38,7 +49,7 @@ const Home = () => {
           value={url}
           placeholder='Enter URL'
         />
-        <Button onClick={getData}>Submit</Button>
+        <Button onClick={getData} className="px-6">Submit</Button>
       </div>
       {isSubmitted && (
         <div className='flex items-center justify-center'>
